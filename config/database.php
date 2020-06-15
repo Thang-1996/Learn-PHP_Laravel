96cbe166bcd533f153a2aa8041130772581fb635
Nguyễn Minh Thắng <minhthang009@gmail.com>
2020-06-15 10:35:50 +0700

    asdasd

--- config/database.php
+++ config/database.php
@@ -86,7 +86,7 @@ return [
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
+            'charset' => 'utf8mb4',
             'prefix' => '',
             'prefix_indexes' => true,
         ],
